89fc5387455b308ea9ef429db5d585f8a5c41b47
Farafonov_Egor_Aleksandrovich <367578@edu.itmo.ru>
2025-01-13 14:27:26 +0300

    Making positions abcolute so that changing window whont hurt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const plane = document.getElementById('plane');
-let lastPoint = null; // Сохранение координат последней точки
+let lastPoint = null;
 const toggleColorButton = document.getElementById('toggle-color');
 const refreshInput = document.getElementById('refresh-interval');
 const setIntervalButton = document.getElementById('set-interval');
@@ -9,11 +9,8 @@ let currentColor = 'blue';
 let refreshIntervalId = null;
 
 plane.addEventListener('click', (event) => {
-  const bodyRect = document.body.getBoundingClientRect()
-  const rect = plane.getBoundingClientRect();
-
-  const x = event.offsetX + rect.left - bodyRect.left;
-  const y = event.offsetY + rect.top - bodyRect.top;
+  const x = event.offsetX;
+  const y = event.offsetY;
   console.log(`x = ${x}, y = ${y} plx = ${plane.offsetX}`)
 
   const point = document.createElement('div');
@@ -40,13 +37,13 @@ toggleColorButton.addEventListener('click', () => {
 function drawLine(x1, y1, x2, y2) {
   const dx = x2 - x1;
   const dy = y2 - y1;
-  const length = Math.sqrt(dx * dx + dy * dy); // Длина линии
-  const angle = Math.atan2(dy, dx) * (180 / Math.PI); // Угол линии в градусах
+  const length = Math.sqrt(dx * dx + dy * dy);
+  const angle = Math.atan2(dy, dx) * (180 / Math.PI);
 
   const line = document.createElement('div');
   line.className = 'line';
   line.style.width = `${length}px`;
-  line.style.height = '2px'; // Толщина линии
+  line.style.height = '2px';
   line.style.left = `${x1}px`;
   line.style.top = `${y1}px`;
   line.style.transform = `rotate(${angle}deg)`;
@@ -59,12 +56,10 @@ setIntervalButton.addEventListener('click', () => {
   const interval = parseInt(refreshInput.value, 10);
 
   if (!isNaN(interval) && interval > 0) {
-    // Очищаем предыдущий интервал
     if (refreshIntervalId) {
       clearInterval(refreshIntervalId);
     }
 
-    // Устанавливаем новый интервал
     refreshIntervalId = setInterval(() => {
       location.reload();
     }, interval * 1000);
@@ -75,12 +70,3 @@ setIntervalButton.addEventListener('click', () => {
   }
 });
 
-// Функция для генерации случайного цвета
-/*function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}*/
